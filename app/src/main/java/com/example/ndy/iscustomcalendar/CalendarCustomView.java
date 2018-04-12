c877374f91cdbda02664b1edb515c6b662f5784a
Ndy <viprec@yahoo.com>
2018-04-12 00:25:19 -0400

    gitignore

---
+++ app/src/main/java/com/example/ndy/iscustomcalendar/CalendarCustomView.java
@@ -0,0 +1,10 @@
+package com.example.ndy.iscustomcalendar;
+
+import android.widget.LinearLayout;
+
+/**
+ * Created by NDY on 4/11/2018.
+ */
+
+public class CalendarCustomView extends LinearLayout {
+}
